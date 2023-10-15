ac120ec12daecf7976915d31e86e957971bdd73b
rithvij425 <147691986+rithvij425@users.noreply.github.com>
2023-10-15 12:16:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+// prompt("enter your name","");
+// console.log("this is console");
+// console.log(6+5);
+// document.write("<h1> javascript</h1>");
+
+//################
+
+// var num1=5;
+// var num1=6 //we can redeclare the variable
+// num1=8 //#we can reassign the value
+// console.log(num1);
+
+// let num2 =9
+// // let num2 =10 #we cant redeclare the variable
+// num2 =11 //#we can reassign the value
+// console.log(num2);
+
+// const num3=10
+// // const num3=11 #we cant redeclare the variable
+// // num3=15 #we cant reassign the value
+// console.log(num3);
+
+//#############
+
+
+// let num1=5
+// let num2="5"
+// console.log(num1==num2);
+// console.log(num1===num2);
+
+//##############
+
+// function mul(n1,n2,n3,n4){
+//     return n1*n2*n3*n4;
+// }
+
+// document.write(mul(4,5,6,7));
+
+// function greet() {
+//     alert("hello everyone")
+// }
+
+//#############
+
+// let age=10;
+// if(age>=18)
+// {
+//     document.write("eligible for vote");
+// }
+// else{
+//     document.write("not eligible");
+// }
+
+//#############
+
+// for (let num=10; num>=1;num--){
+//     console.log(num);
+// }
+
+//############
+
+ let ele = document.querrySelector("#head").innerHTML;
+ console.log(ele);
